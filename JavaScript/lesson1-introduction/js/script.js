ed0d7190720627bc16af04d36c23bb5aae24f51c
Hasan Bakhtiar <hasan.doctype@gmail.com>
2022-11-09 16:57:33 +0400

    js

---
+++ JavaScript/lesson1-introduction/js/script.js
@@ -0,0 +1,13 @@
+// one line
+/*
+more 
+line 
+*/
+// document.write('hello new data');
+// debugger;
+
+// console.log("Hello1");
+// console.log("Hello2");
+// console.loga("Hello3");
+// console.log("Hello4");
+// console.log("Hello5");
---
+++ JavaScript/lesson2-dataTypes/js/script.js
@@ -0,0 +1,41 @@
+// var info = 5;
+// var data = 10;
+// console.log(info*data);
+
+// Primitive Data types
+// string
+// var textOne = "Hello ";
+// var textTwo = "JavaScript";
+// console.log(textOne + textTwo);
+
+// number
+// var numOne = 5;
+// var numTwo = 10;
+// console.log(numOne + numTwo);
+
+// boolean
+// true
+// false
+// if (false) {
+//     console.log('success');
+// }else{
+//     console.log('reject');
+// }
+
+// Composite Data types
+// array
+// var arrInfo = ['hello',5,true];
+// console.log(arrInfo);
+// object
+// var objInfo={
+//     name:"Shabnam",
+//     surname:"Aliyeva",
+//     birthyear:2003
+// }
+// console.log(objInfo.surname);
+
+// Special Data types
+//undefined
+// var a = null;
+// console.log(a);
+// null
