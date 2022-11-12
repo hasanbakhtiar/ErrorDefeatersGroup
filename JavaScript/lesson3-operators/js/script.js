b37f6d9a02f883db620198d5759ca698df669279
Hasan Bakhtiar <hasan.doctype@gmail.com>
2022-11-12 15:00:19 +0400

    operators

---
+++ JavaScript/lesson3-operators/js/script.js
@@ -0,0 +1,74 @@
+// Assignment operators
+// =
+// +=
+// -=
+// *=
+// /=
+// %=
+// **=
+
+// arithmetic operators
+// +
+// -
+// *
+// /
+// %
+// **
+// ++
+// --
+
+// comparative operators
+// ==
+// ===
+// <
+// <=
+// >
+// >=
+// !=
+// !==
+
+// logical operators
+// && - and
+// || - or
+// ! - not
+
+// string operators
+// +
+// +=
+
+
+// Special Operators
+// --------------------------------------------------------------------------------------------------
+// Other (Special) Operators (Xüsusi operatorlar) Start
+
+// ,      (vergül)(kodları bir-birindən ayırmaq və sintaksis qanun uyğunluqları icra etmək üçündür )
+
+// \            (tərs slash)(Qaçındırma operatorudur)
+
+// typeof       (tip üçün)(datanın tipini təyin etmək üçündür)
+
+// ?:           (sual üst-üstə qoşa nöqtə)((şərt) ? ifadə1 : ifadə2)
+// (5 >= 10) ? document.write("Hi") : document.write("Bye");
+// şərt            ? true                 : false
+
+// void         (dəyişkənin dəyərini undefined etmək üçündür)
+
+
+// delete       (objectin dəyərini silmək üçündür)
+
+// new          (yeni dəyər yaratməq üçündür)
+
+// this         (mövcud dəyəri işarələmək üçündür)
+
+
+// in           (dəyişkənin daxilində dəyərin olub olmamasını yoxlamaq üçündür)
+// instanceof   (dəyişkənin içində xüsusiyyət olub olmamasını yoxlamaq üçündür)
+
+
+
+
+
+    
+
+// Other (Special) Operators (Xüsusi operatorlar) End
+// --------------------------------------------------------------------------------------------------
