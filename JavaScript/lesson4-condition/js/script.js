db5c4d318143cf21787c9db75ddca42b1a8a28c1
Hasan Bakhtiar <hasan.doctype@gmail.com>
2022-11-16 17:00:08 +0400

    js

---
+++ JavaScript/lesson4-condition/js/script.js
@@ -0,0 +1,51 @@
+
+
+// 1.Dəyişkən adları mənalı və anlaşıqlı olmalıdır.
+// 2.Dəyişkən adlarını yazarkən İngilis əlifbasından başqa əlifadan istifadə etmək qadağandır.
+// 3.Dəyişkən adları _ (alt xətt) ilə başlaya bilər.
+// 4.Dəyişkən adları $(dollar) işarəsi ilə başlaya bilər.
+// 5.Dəyişkən adları heç bir zaman rəqəm ilə başlaya bilməz.Amma ilk hərfdə sonra istifadə edə bilərik.
+// 6.Dəyişkən adları böyük kiçik hərflərə həssasdır.
+// 7.Eyni dəyişkənə iki fərqli məlumat tanımlanan zaman sonuncu tanımlanan məlumatı göstərir.
+// 8.JavaScript tərəfindən hazırlanmış rezerv kəlimələri dəyişkən adlarına vermək qadağandır.
+// 9.Eyni dəyişkən adını dəişkən ifadələrində istifadə etmək qadağandır.
+
+// abstract 	    arguments	    await*	            boolean
+// break	        byte	        case	            catch
+// char	            class*	        const	            continue
+// debugger	        default	        delete	            do
+// double	        else	        enum*	            eval
+// export*	        extends*	    false	            final
+// finally	        float	        for	                function
+// goto	            if	            implements	        import*
+// in	            instanceof	    int	                interface
+// let*	            long	        native	            new
+// null	            package	        private	            protected
+// public	        return	        short	            static
+// super*	        switch	        synchronized	    this
+// throw	        throws	        transient	        true
+// try	            typeof	        var	                void
+// volatile	        while	        with	            yield
+
+
+// var   -> variable -> global
+// let   -> let      -> local
+// const -> constant -> local
+
+// global 
+// if (true) {
+//     // local global
+//     if (true) {
+//         var a = 5;
+//         // local
+//     }
+//     console.log(a);
+// }
+
+// condition
+
+// if - eger
+// else - deyilse
+// else if - deyilse eger
+
+
