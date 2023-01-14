c534acb724863aaab0c69d474ac7f58641045e3d
Hasan Bakhtiar <hasan.doctype@gmail.com>
2023-01-14 14:43:45 +0400

    components

---
+++ ReactJS/lesson3-components/src/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import Header from './components/Header'
+import Hero from './components/Hero'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Hero />
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ ReactJS/lesson3-components/src/components/Hero.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import imgOne from '../assets/img/unsplash1.jpg'
+import imgTwo from '../assets/img/unsplash2.jpg'
+class Hero extends Component {
+  render() {
+    return (
+   <div id="carouselExampleIndicators" className="carousel slide">
+  <div className="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to={0} className="active" aria-current="true" aria-label="Slide 1" />
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to={1} aria-label="Slide 2" />
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to={2} aria-label="Slide 3" />
+  </div>
+  <div className="carousel-inner">
+    <div className="carousel-item active">
+      <img src={imgOne} className="d-block w-100" alt="..." />
+    </div>
+    <div className="carousel-item">
+      <img src={imgTwo} className="d-block w-100" alt="..." />
+    </div>
+    <div className="carousel-item">
+      <img src={imgOne} className="d-block w-100" alt="..." />
+    </div>
+  </div>
+  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true" />
+    <span className="visually-hidden">Previous</span>
+  </button>
+  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true" />
+    <span className="visually-hidden">Next</span>
+  </button>
+</div>
+
+    )
+  }
+}
+
+export default Hero
